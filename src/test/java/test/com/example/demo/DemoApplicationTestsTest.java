2007ab64e848f47b42c62b20035a77d6d14bf223
马晨旭 <machenxu@machenxudeMacBook-Pro.local>
2019-04-21 21:33:57 +0800

    Initial commit

---
+++ src/test/java/test/com/example/demo/DemoApplicationTestsTest.java
@@ -0,0 +1,55 @@
+package test.com.example.demo; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* DemoApplicationTests Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Apr 20, 2019</pre> 
+* @version 1.0 
+*/ 
+public class DemoApplicationTestsTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: contextLoads() 
+* 
+*/ 
+@Test
+public void testContextLoads() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setUp() 
+* 
+*/ 
+@Test
+public void testSetUp() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getHello() 
+* 
+*/ 
+@Test
+public void testGetHello() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
